71c08f3f94db9bc717ea7882ee81705ac2b8dcd4
Ginver <74971813+Ginver@users.noreply.github.com>
2021-03-18 15:46:32 +0100

    labels and buttons added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,44 @@ import './App.css';
 function App() {
   return (
     <>
+        <div>
       <h1>Fruitmand bezorgservice</h1>
+        <form>
+            <label>Aardbei
+                <button>
+
+                </button>
+
+                <button>
+
+                </button>
+            </label>
+            <label>Banaan
+                <button>
+
+                </button>
+                <button>
+
+                </button>
+            </label>
+            <label>Appel
+                <button>
+
+                </button>
+                <button>
+
+                </button>
+            </label>
+            <label>Kiwi
+                <button>
+
+                </button>
+                <button>
+
+                </button>
+            </label>
+        </form>
+        </div>
     </>
   );
 }
